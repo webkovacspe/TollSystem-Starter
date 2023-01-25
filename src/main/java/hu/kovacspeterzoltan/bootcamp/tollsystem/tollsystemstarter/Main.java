c1bebf8e1eca32bfd49da82bd2a08aa72e099a3c
webkovacs <webkovacs@gmail.com>
2023-01-25 15:56:27 +0100

    VehicleRegister Plug-In beépítése

--- src/main/java/hu/kovacspeterzoltan/bootcamp/tollsystem/tollsystemstarter/Main.java
+++ src/main/java/hu/kovacspeterzoltan/bootcamp/tollsystem/tollsystemstarter/Main.java
@@ -4,25 +4,32 @@ import hu.kovacspeterzoltan.bootcamp.tollsystem.tollsystemconsoleui.controller.T
 import hu.kovacspeterzoltan.bootcamp.tollsystem.tollsystemconsoleui.presenter.TollSystemAppPresenterImp;
 import hu.kovacspeterzoltan.bootcamp.tollsystem.tollsystemconsoleui.view.TollSystemConsoleUIView;
 import hu.kovacspeterzoltan.bootcamp.tollsystem.tollsystempersistencestorage.TollSystemPersistenceStorage;
+import hu.kovacspeterzoltan.bootcamp.tollsystem.vehicleregisterplugin.VehicleRegisterFindIteractor;
+import hu.kovacspeterzoltan.bootcamp.tollsystem.vehicleregisterplugin.presenter.VehicleRegisterPresenterImp;
 import hu.kovacspeterzoltan.bootcamp.tollsystemapp.MotorwayVignetteInteractor;
 import hu.kovacspeterzoltan.bootcamp.tollsystemapp.api.MotorwayVignettePresenterInterface;
-import hu.kovacspeterzoltan.bootcamp.tollsystemapp.presenter.VehicleRegisterPresenterImp;
 import hu.kovacspeterzoltan.bootcamp.tollsystemapp.storage.MotorwayVignetteStorageInterface;
 import hu.kovacspeterzoltan.bootcamp.vehiclepersistencestorage.PersistenceStorageCSV;
 import hu.kovacspeterzoltan.bootcamp.vehicleregister.VehicleInteractor;
+import hu.kovacspeterzoltan.bootcamp.vehicleregister.api.VehicleRegisterPresenterInterface;
 import hu.kovacspeterzoltan.bootcamp.vehicleregister.storage.VehicleRegisterStorageInterface;
 
 public class Main {
     public static void main(String[] args) {
-        MotorwayVignetteInteractor motorwayVignetteInteractor = new MotorwayVignetteInteractor();
-
         VehicleInteractor vehicleInteractor = new VehicleInteractor();
+
         VehicleRegisterStorageInterface vehicleRegisterStorage = new PersistenceStorageCSV();
         vehicleInteractor.setStorageImp(vehicleRegisterStorage);
+
         VehicleRegisterPresenterImp vehicleRegisterPresenter = new VehicleRegisterPresenterImp();
         vehicleInteractor.setPresenterImp(vehicleRegisterPresenter);
-        motorwayVignetteInteractor.setVehicleRegisterImp(vehicleInteractor);
-        motorwayVignetteInteractor.setVehicleRegisterPresenterImp(vehicleRegisterPresenter);
+
+        VehicleRegisterFindIteractor vehicleRegisterPlugIn = new VehicleRegisterFindIteractor();
+        vehicleRegisterPlugIn.setVehicleRegisterFindImp(vehicleInteractor);
+        vehicleRegisterPlugIn.setVehicleRegisterPresenterImp(vehicleRegisterPresenter);
+
+        MotorwayVignetteInteractor motorwayVignetteInteractor = new MotorwayVignetteInteractor();
+        motorwayVignetteInteractor.setVehicleRegisterPlugInImp(vehicleRegisterPlugIn);
 
         MotorwayVignetteStorageInterface motorwayVignetteStorage = new TollSystemPersistenceStorage();
         motorwayVignetteInteractor.setStorageImp(motorwayVignetteStorage);
